41fac7c8a9b091ab0c54b8ec3c57d58058609e0b
Gabriele Mainetti <mg02.gabriele@gmail.com>
2023-01-12 18:31:03 +0100

    menu page

---
+++ resources/views/components/menu.blade.php
@@ -0,0 +1,14 @@
+<nav>
+    <ul class="d-flex list-unstyled justify-content-around">
+        <li>CHARACTERS</li>
+        <li>COMICS</li>
+        <li>MOVIES</li>
+        <li>TV</li>
+        <li>GAMES</li>
+        <li>COLLECTIBLES</li>
+        <li>VIDEOS</li>
+        <li>FANS</li>
+        <li>NEWS</li>
+        <li>SHOP</li>
+    </ul>
+</nav>
