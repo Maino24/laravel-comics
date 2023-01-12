81c90da1ae2c059b59186bf7e966591562157c85
Gabriele Mainetti <mg02.gabriele@gmail.com>
2023-01-12 18:12:38 +0100

    nav

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,3 +2,19 @@
 
 @section('page-title', "Welcome Comics")
     
+@section('header-content')
+    <nav>
+        <ul class="d-flex list-unstyled justify-content-around">
+            <li>CHARACTERS</li>
+            <li>COMICS</li>
+            <li>MOVIES</li>
+            <li>TV</li>
+            <li>GAMES</li>
+            <li>COLLECTIBLES</li>
+            <li>VIDEOS</li>
+            <li>FANS</li>
+            <li>NEWS</li>
+            <li>SHOP</li>
+        </ul>
+    </nav>
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,14 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>@yield('page-title')</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">    <title>@yield('page-title')</title>
 </head>
 <body>
 
     <header>
-
+        @yield('header-content')
     </header>
-
+    <header>
+        @yield('main-content')
+    </header>
+    <footer>
+        @yield('footer-content')
+    </footer>
        
 </body>
 </html>
