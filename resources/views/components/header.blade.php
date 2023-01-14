7ce88c7a6a0a83a4c49c7d7fa4a5fd6bd63338c3
Gabriele Mainetti <mg02.gabriele@gmail.com>
2023-01-14 18:19:18 +0100

    header

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,40 +1,47 @@
-<nav>
-    
-    <a href="{{ route('home') }}">
-        <img src="{{ asset('img/dc-logo.png')}}" alt="">
-    </a>
-    
+<div class="containerHeader">
+    <nav class="headerTop">
+        
+        <a href="{{ route('home') }}">
+            <img src="{{ asset('img/dc-logo.png')}}" alt="">
+        </a>
+        
 
-    <ul>
-        <li>
-            <a href="{{ route('home') }}">HOME</a>
-        </li>
-        <li>
-            <a href="{{ route('index.comics') }}">COMICS</a>
-        </li>
-        <li>
-            <a href="">MOVIES</a>
-        </li>
-        <li>
-            <a href="">TV</a>
-        </li>
-        <li>
-            <a href="">GAMES</a>
-        </li>
-        <li>
-            <a href="">COLLECTIBLES</a>
-        </li>
-        <li>
-            <a href="">VIDEOS</a>
-        </li>
-        <li>
-            <a href="">FANS</a>
-        </li>
-        <li>
-            <a href="">NEWS</a>
-        </li>
-        <li>
-            <a href="">SHOP</a>
-        </li>
-    </ul>
-</nav>
+        <ul class="list">
+            <li>
+                <a href="{{ route('home') }}">HOME</a>
+            </li>
+            <li>
+                <a href="{{ route('index.comics') }}">COMICS</a>
+            </li>
+            <li>
+                <a href="">MOVIES</a>
+            </li>
+            <li>
+                <a href="">TV</a>
+            </li>
+            <li>
+                <a href="">GAMES</a>
+            </li>
+            <li>
+                <a href="">COLLECTIBLES</a>
+            </li>
+            <li>
+                <a href="">VIDEOS</a>
+            </li>
+            <li>
+                <a href="">FANS</a>
+            </li>
+            <li>
+                <a href="">NEWS</a>
+            </li>
+            <li>
+                <a href="">SHOP</a>
+            </li>
+        </ul>
+
+    </nav>
+</div>
+
+<div class="background-img">
+
+</div>
--- resources/views/pages/comics/index.blade.php
+++ resources/views/pages/comics/index.blade.php
@@ -8,14 +8,14 @@
     <div class="container">
         <div class="row">
             @foreach ($comics as $elem)
-                <div class="col-3">
-                    <div class="card" style="width: 18rem;">
+                <div class="col-2">
+                    <div class="card" style="width: 10rem;">
                         <a href="">
                             <img src="{{ $elem['thumb']}}" class="card-img-top" alt="...">
                         </a>
                         <div class="card-body">
                         <h5 class="card-title">{{ $elem['title']}}</h5>
-                        <p class="card-text">{{ $elem['description']}}</p>
+                        <!--<p class="card-text">{{ $elem['description']}}</p>-->
                         </div>
                     </div>
                 </div>
